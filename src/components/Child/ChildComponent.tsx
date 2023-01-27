9b06da89802caa6cf34b4ca4e8347f88fe8df062
Abdelrahman Tarek <abdelrahman.tarek.omar@gmail.com>
2023-01-27 22:43:13 +0200

    bassem's way more easier

--- src/components/Child/ChildComponent.tsx
+++ src/components/Child/ChildComponent.tsx
@@ -1,38 +1,21 @@
 import React, { useState } from "react";
 import styles from "./child.module.css";
-// {
-//     name: string;
-//     accountNumber: string;
-//     amount: string;
-//     currency: string;
-//   };
+
 interface Iprops {
   labelsData: any;
 
   valuesData: any;
 }
 const ChildCompomemt = ({ labelsData, valuesData }: Iprops) => {
-  const labelArr = Object.entries(labelsData);
-  const valueArr = Object.entries(valuesData);
-  const testArr = [];
-  const finalArr = valueArr.map((val) =>
-    labelArr.map((label) =>
-      label[0] === val[0] ? `${label[1]}:${val[1]}` : false
-    )
-  );
-  for (let i = 0; i < finalArr.length; i++) {
-    const insideArray = finalArr[i];
-    testArr.push(insideArray.filter((el: any) => el !== false));
+  let result = [];
+  for (let key in labelsData) {
+    result.push([labelsData[key], valuesData[key]]);
   }
-  const arrayToLoop = testArr
-    .filter((item: any) => item.length > 0)
-    .map((test: any) => test[0].split(":"));
-  console.log(arrayToLoop);
   return (
     <div>
       <h1>child component</h1>
-      {arrayToLoop.map((cell: any) => (
-        <div className="cell">
+      {result.map((cell: any) => (
+        <div className="cell" key={cell[0]}>
           <div className={styles.cellTitle}>{cell[0]}</div>
           <div className={styles.cellContent}>{cell[1]}</div>
         </div>
